8b2c41327c90edefc369262482d0065d25fe3f38
VaheStepanyan100 <vahestepanyan100@gmail.com>
2022-10-25 14:36:17 +0400

    First-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import AddTodo from './components/AddTodo/AddTodo';
+import TaskList from './components/TaskList/TaskList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todos, setTodos] = useState(initialTodos);
+
+  function handleAddTodo(title) {
+    setTodos([
+      ...todos,
+      {
+        id: nextId++,
+        title: title,
+        done: false
+      }
+    ]);
+  }
+
+  function handChangeTodo(nextTodo) {
+    setTodos(todos.map(todo => {
+      if (todo.id === nextTodo.id) {
+        return nextTodo;
+      } else {
+        return todo;
+      }
+    }));
+  }
+
+  function handleDeleteTodo(todoId) {
+    setTodos(todos.filter(todo => todo.id !== todoId));
+  }
+
+  function handleDeleteMarked() {
+    setTodos(todos.filter(todo => !todo.done));
+  }
+
+   return (
+    <div>
+      <AddTodo
+        onAddTodo={handleAddTodo}
+      />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+      <button onClick={handleDeleteMarked}>Delete Marked</button>
     </div>
-  );
+   );
 }
+
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: 'Learn JS', done: true },
+  { id: 1, title: 'Learn React', done: false },
+  { id: 2, title: 'Learn TypeScript', done: false }
+]
 
 export default App;
